3ee2a4591d03da6409560d7c349f5dac0bf4f246
franzap <Francesco.zappacosta96@gmail.com>
2021-12-31 20:24:08 +0100

    metodi aggiunti nelle classi
    aggiunti i metodi nelle classi dao (sono ancora tutti da implementare)

--- DAO/implementazioniDao/AutomobileHtDao.java
+++ DAO/implementazioniDao/AutomobileHtDao.java
@@ -1,5 +1,40 @@
 package Concessionario.DAO.implementazioniDao;
 
-public class AutomobileHtDao {
+import java.util.ArrayList;
 
+import Concessionario.DAO.interfacceDao.Dao;
+import Concessionario.entita.Automobile;
+
+public class AutomobileHtDao implements Dao<Automobile>{
+
+	@Override
+	public Automobile get(long l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Automobile> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Automobile t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Automobile t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(long l) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- DAO/implementazioniDao/PersonaHtDao.java
+++ DAO/implementazioniDao/PersonaHtDao.java
@@ -1,5 +1,39 @@
 package Concessionario.DAO.implementazioniDao;
 
-public class PersonaHtDao {
+import Concessionario.entita.Persona;
+import Concessionario.DAO.interfacceDao.Dao;
+import java.util.ArrayList;
+
+public class PersonaHtDao implements Dao<Persona>{
+
+	@Override
+	public Persona get(long l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Persona> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Persona t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Persona t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(long l) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
