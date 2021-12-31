ed25c0a611cb689377c80c62b227f2d0f77cb601
franzap <Francesco.zappacosta96@gmail.com>
2021-12-31 18:57:36 +0100

    creazione file dao

--- DAO/Dao.java
+++ DAO/Dao.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public interface Dao <T>{
 	
-		public T get(int id);
-	    public List<T> findAll();
-	    public void save(T t);
-	    public void update(T t);
-	    public void delete(int id);
+	public T get(int id);
+	public List<T> findAll();
+	public void save(T t);
+	public void update(T t);
+	public void delete(int id);
 
 }
