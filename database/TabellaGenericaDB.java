5e517b27ba6bd9262b97d9c73bafe2bf2a56151c
franzap <Francesco.zappacosta96@gmail.com>
2022-01-01 19:14:36 +0100

    implementazione db
    implementati metodi e classi per simulare il database

--- database/TabellaGenericaDB.java
+++ database/TabellaGenericaDB.java
@@ -1,5 +1,10 @@
 package Concessionario.database;
 
-public interface TabellaGenericaDB {
+public interface TabellaGenericaDB <T>{
+	
+	public void create(T t);
+	public T read(long id);
+	public void update(long id, T o);
+	public void delete(long id);	
 	
 }
--- database/TableVeicolo.java
+++ database/TableVeicolo.java
@@ -1,5 +1,47 @@
 package Concessionario.database;
 
-public class TableVeicolo {
+import java.util.Hashtable;
 
+import Concessionario.entita.Veicolo;
+
+public class TableVeicolo implements TabellaGenericaDB<Veicolo>{
+	private Hashtable <Long,Veicolo> table;
+	private static long idx;
+
+	@Override
+	public Veicolo read(long id) {
+		if(!this.table.containsKey(id)) {
+			System.out.println("Il db non contiene veicoli con questo id!!");
+			return null;
+		}
+		return this.table.get(id);
+	}
+
+	@Override
+	public void update(long id, Veicolo v) {
+		if(!this.table.containsKey(id)) {
+			System.out.println("Il db non contiene veicoli con questo id!!");
+			return;
+		} 
+		this.table.put(id, v);
+	}
+
+	@Override
+	public void delete(long id) {
+		if(!this.table.containsKey(id)) {
+			System.out.println("Il db non contiene veicoli con questo id!!");
+			return;
+		}
+		this.table.remove(id);
+	}
+
+
+	@Override
+	public void create(Veicolo v) {
+		if(this.table.contains(v)) {
+			System.out.println("Stai aggiungendo una veicoli che già esiste nel db");
+			return;
+		}
+		this.table.put(++idx,v);
+	}
 }
